70e1acccb6aa644e5158363000899075d355c8fa
lucascostamr <lucascostamr812@gmail.com>
2025-05-14 21:52:34 -0300

    feat: calculate funcao heuristica and get the shortest path

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-def encontrar_coordenadas_iniciais(identificador: str, tabuleiro: list[list[str]]) -> tuple[int, int]:
+from math import hypot
+
+
+def encontrar_coordenadas(
+    identificador_personagem: str,
+    identificador_chegada: str,
+    tabuleiro: list[list[str]],
+) -> tuple[int, int]:
     num_linhas = len(tabuleiro)
     num_coluas = len(tabuleiro[0])
 
+    coordenada_personagem = None
+    coordenada_chegada = None
+
     for i in range(num_linhas):
         for j in range(num_coluas):
-            if tabuleiro[i][j] == identificador:
-                return (i, j)
+            if tabuleiro[i][j] == identificador_personagem:
+                coordenada_personagem = (i, j)
+            if tabuleiro[i][j] == identificador_chegada:
+                coordenada_chegada = (i, j)
+
+    return (coordenada_personagem, coordenada_chegada)
+
+
+def cacular_funcao_heuristica(
+    coordenas_personagem, coordenadas_chegada, distancia_percorrida
+):
+    x_personagem, y_personagem = coordenadas_personagem
+    x_chegada, y_chegada = coordenadas_chegada
+
+    hipotenusa = hypot(abs(x_personagem - x_chegada), abs(y_personagem - y_chegada))
+
+    return hipotenusa + distancia_percorrida
+
 
 def movimento_valido(coordenadas, tabuleiro):
     num_linhas = len(tabuleiro)
@@ -13,9 +39,22 @@ def movimento_valido(coordenadas, tabuleiro):
 
     x, y = coordenadas
 
-    if x >= 0 and y >= 0 and x < num_linhas -1 and y < num_coluas -1:
+    if x >= 0 and y >= 0 and x < num_linhas - 1 and y < num_coluas - 1:
         return True
     return False
+
+def retorna_menor_caminho(lista_caminhos):
+    if not lista_caminhos:
+        return None
+
+    menor_caminho = lista_caminhos[0]
+    for caminho in lista_caminhos[1:]:
+        if caminho.funcao_heuristica < menor_caminho.funcao_heuristica:
+            menor_caminho = caminho
+
+    lista_caminhos.remove(menor_caminho)
+    return menor_caminho
+
 
 tab: list[list[str]] = [
     ["C", "_", "_", "_", "B", "_"],
@@ -26,7 +65,7 @@ tab: list[list[str]] = [
     ["_", "_", "_", "_", "_", "S"],
 ]
 
-#Calcular a funcao heuristica ao redor
+# Calcular a funcao heuristica ao redor
 
 movimentos: dict[str, tuple[int, int]] = {
     "cima": (0, 1),
@@ -36,20 +75,31 @@ movimentos: dict[str, tuple[int, int]] = {
     "diagonal_inferior_direita": (1, -1),
     "diagonal_inferior_esquerda": (-1, -1),
     "diagonal_superior_direita": (1, 1),
-    "diagonal_superior_esquerda": (-1, 1)
+    "diagonal_superior_esquerda": (-1, 1),
 }
 
-coordenadas_personagem: tuple[int, int] = encontrar_coordenadas_iniciais(identificador="C", tabuleiro=tab)
+distancia_diagonal = {False: 1, True: 1.4}
+
+coordenadas_personagem, coordenadas_chegada = encontrar_coordenadas(
+    identificador_personagem="C", identificador_chegada="S", tabuleiro=tab
+)
 
 lista_caminhos = []
 
+
 class Caminho:
-    def __init__(self, funcao_heuristica, coordenada):
+    def __init__(self, funcao_heuristica, coordenada, distancia_percorrida):
+        self.coordenadas_percorridas = []
         self.funcao_heuristica = funcao_heuristica
-        self.coordenada = coordenada
+        self.distancia_percorrida = distancia_percorrida
+
+        self.coordenadas_percorridas.append(coordenada)
 
     def __repr__(self):
-        return f"Caminho (funcao_heuristica={self.funcao_heuristica}, coordenada={self.coordenada})"
+        return f"Caminho (funcao_heuristica={self.funcao_heuristica}, coordenadas={self.coordenadas_percorridas}, distancia_percorrida={self.distancia_percorrida})"
+
+
+menor_caminho = Caminho(0, None, 0)
 
 for key in movimentos.keys():
     movimento_x, movimento_y = movimentos[key]
@@ -57,7 +107,30 @@ for key in movimentos.keys():
 
     nova_coordenada = (movimento_x + x_atual, movimento_y + y_atual)
     if movimento_valido(nova_coordenada, tab):
-        novo_caminho = Caminho(0, nova_coordenada)
+        distancia = key in [
+            "diagonal_inferior_direita",
+            "diagonal_inferior_esquerda",
+            "diagonal_superior_direita",
+            "diagonal_superior_esquerda",
+        ]
+
+        distancia_percorrida = (
+            distancia_diagonal[distancia] + menor_caminho.distancia_percorrida
+        )
+
+        funcao_heuristica = cacular_funcao_heuristica(
+            coordenadas_chegada=coordenadas_chegada,
+            coordenas_personagem=coordenadas_personagem,
+            distancia_percorrida=distancia_percorrida,
+        )
+        novo_caminho = Caminho(
+            funcao_heuristica,
+            nova_coordenada,
+            distancia_percorrida
+        )
         lista_caminhos.append(novo_caminho)
 
+menor_caminho = retorna_menor_caminho(lista_caminhos)
+
 print(lista_caminhos)
+print(menor_caminho)
